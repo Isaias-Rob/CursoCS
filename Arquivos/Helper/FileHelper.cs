51ed5f1fa5d3215fb6e59350601fe57bb0a6d55f
Isaias-Rob <isaias.roberto.2008@gmail.com>
2022-09-28 12:02:45 -0300

    Comitando ultima aula do curso

--- Arquivos/Helper/FileHelper.cs
+++ Arquivos/Helper/FileHelper.cs
@@ -43,7 +43,58 @@ namespace Arquivos.Helper
         }
 
         public void CriarArquivoTxt(string caminho, string conteudo){
-            File.WriteAllText(caminho,conteudo);
+            if(File.Exists(caminho)){
+                File.WriteAllText(caminho,conteudo);
+            }
+        }
+
+        public void CriarArquivoTxtStream(string caminho, List<string> conteudo){
+            using(var stream = File.CreateText(caminho)){ //limpar a stream e liberar o arquivo por isso o using
+                foreach(var linha in conteudo){
+                    stream.WriteLine(linha);
+                }
+            }
+        }
+
+        public void AppendTexto(string caminho, string conteudo){
+            File.AppendAllText(caminho, conteudo); //se não existir ele cria o arquivo.
+        }
+
+        public void AppendTextoStream(string caminho, List<string> conteudo){
+            using(var stream = File.AppendText(caminho)){ 
+                foreach(var linha in conteudo){
+                    stream.WriteLine(linha);
+                }
+            }
+        }
+
+        public void LerImprimirArquivo(string caminho){
+            var conteudo = File.ReadAllLines(caminho);
+
+            foreach(var linha in conteudo){
+                System.Console.WriteLine(linha);
+            }
+        }
+
+        public void LerImprimirArquivoStream(string caminho){
+            string linha = string.Empty;
+            using(var stream = File.OpenText(caminho)){
+                while((linha = stream.ReadLine()) != null){
+                    System.Console.WriteLine(linha);
+                }
+            }
+        }
+
+        public void MoverArquivo(string source, string destination, bool sobrescrever){
+            File.Move(source, destination, sobrescrever);//por padrao nao sobrescreve arquivos com o mesmo nome
+        }
+
+        public void CopiarArquivo(string source, string destination, bool sobrescrever){
+            File.Copy(source, destination, sobrescrever); //por padrao nao sobrescreve arquivos com o mesmo nome
+        }
+
+        public void DeletarArquivo(string caminho){
+            File.Delete(caminho);
         }
     }
 }
--- Arquivos/Program.cs
+++ Arquivos/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Arquivos.Helper;
 
 var caminho = "C:\\Users\\Isaias\\Documents\\Csharp\\Arquivos";
+var caminho_arquivo = Path.Combine(caminho,"Pasta 3","arquivo_teste.txt");
+var caminho_arquivo2 = Path.Combine(caminho,"Pasta 3","arquivo_teste2.txt");
+var listaString = new List<string> {"Linha 1", "Linha 2", "Linha 3"};
+var listaString2 = new List<string> {"Linha 4", "Linha 5", "Linha 6"};
 
 FileHelper helper = new FileHelper();
 helper.ListarDiretorios(caminho);
@@ -9,6 +13,16 @@ helper.ListarArquivosDiretorios(caminho);
 System.Console.WriteLine("\n\n\n");
 helper.ListarArquivosDiretorios(caminho,"*.txt");
 System.Console.WriteLine("\n\n\n");
-//System.Console.WriteLine("Criando o diretorio: ");
-//helper.CriarDiretorio(Path.Combine(caminho,"Pasta 3", "SubPasta 2"));
+System.Console.WriteLine("Criando o diretorio: ");
+helper.CriarDiretorio(Path.Combine(caminho,"Pasta 3", "SubPasta 2"));
 helper.ApagarDiretorio(Path.Combine(caminho, "Pasta 1","SubPasta 1"),true);
+helper.CriarArquivoTxt(caminho_arquivo, "Teste de escrita de arquivo");
+helper.CriarArquivoTxtStream(caminho_arquivo2,listaString);
+helper.AppendTexto(caminho_arquivo,"\nContinuacao do conteudo...\n Ate mais.");
+helper.AppendTextoStream(caminho_arquivo2, listaString2);
+helper.LerImprimirArquivo(caminho_arquivo);
+helper.LerImprimirArquivoStream(caminho_arquivo2);
+helper.MoverArquivo(caminho_arquivo, Path.Combine(caminho,"Pasta 2","arquivo_novo_nome.txt"));
+helper.CopiarArquivo(Path.Combine(caminho,"Pasta 2","arquivo_novo_nome.txt"),Path.Combine(caminho,"Pasta 3","arquivo_teste.txt"),false);
+helper.DeletarArquivo(Path.Combine(caminho,"Pasta 2","arquivo_novo_nome.txt"));
+
