a97812270c78f9c75ae9903ef6fca2743bd57a64
Isaias-Rob <isaias.roberto.2008@gmail.com>
2022-10-05 11:34:02 -0300

    comitando try catch

--- Exceptions/Aula_1/Program.cs
+++ Exceptions/Aula_1/Program.cs
@@ -1,5 +1,8 @@
 ﻿
 static double Dividir(int x, int y){
+    if(y == 0){
+        throw new ArithmeticException();
+    }
     return (x/y);
 }
 
@@ -7,7 +10,17 @@ static double Dividir(int x, int y){
 int a = 100, b = 0;
 double resultado = 0;
 //ira gerar uma interrupção
-resultado = Dividir(a,b);
-System.Console.WriteLine("{0} dividido por {1} = {2}",a,b,resultado);
+try{
+    resultado = Dividir(a,b);
+    System.Console.WriteLine("{0} dividido por {1} = {2}",a,b,resultado);
+} catch(DivideByZeroException exception){
+    System.Console.WriteLine(exception.Message);
+} catch(Exception exception){
+    System.Console.WriteLine("Exceção genérica: ");
+    System.Console.WriteLine(exception.Message);
+    throw;
+} finally{
+    System.Console.WriteLine("Dividindo ou não, aqui estou");
+}
 
 
